69a90d9609988fbadb6e7c8444abd28340f5cae1
DannyDev <dhernandezm110@gmail.com>
2018-07-05 20:08:46 -0700

    added javascrtipt to hide and display portfolio details

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+document.getElementById("c1").addEventListener("click", function(){
+    document.getElementById("c1").className += " hide";
+});
+document.getElementById("t1").addEventListener("click", function(){
+    document.getElementById("c1").classList.remove("hide");
+});
+document.getElementById("c2").addEventListener("click", function(){
+    document.getElementById("c2").className += " hide";
+});
+document.getElementById("t2").addEventListener("click", function(){
+    document.getElementById("c2").classList.remove("hide");
+});
+document.getElementById("c3").addEventListener("click", function(){
+    document.getElementById("c3").className += " hide";
+});
+document.getElementById("t3").addEventListener("click", function(){
+    document.getElementById("c3").classList.remove("hide");
+});
+document.getElementById("c4").addEventListener("click", function(){
+    document.getElementById("c4").className += " hide";
+});
+document.getElementById("t4").addEventListener("click", function(){
+    document.getElementById("c4").classList.remove("hide");
+});
